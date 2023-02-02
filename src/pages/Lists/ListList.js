3acacf9f8dc24a9ce7bdb86d36943ae08b74d286
uedi <uedi@users.noreply.github.com>
2023-02-02 17:17:44 +0200

    add todo status to lists

--- src/pages/Lists/ListList.js
+++ src/pages/Lists/ListList.js
@@ -2,6 +2,32 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Box, Typography } from '@mui/material'
 
+const ListListItem = ({ list, handleClick }) => {
+    const todosCount = list.todos ? list.todos.length : 0
+    const doneCount = list.todos ? list.todos.reduce((sum, cur) => sum + (cur.done ? 1 : 0), 0) : 0
+    const progress = todosCount > 0 ? doneCount / todosCount * 100 : 0
+    return (
+        <Box
+            onClick={() => handleClick(list.id)}
+            sx={{
+                backgroundColor: 'white',
+                padding: 1,
+                borderRadius: 5,
+                marginBottom: 2,
+                '&:hover': {
+                    backgroundColor: '#f5f5f5'
+                }
+            }}
+            >
+            <Typography
+                sx={{ fontWeight: 'bold' }}
+            >{list.name}</Typography>
+            <Typography>Done todos: {doneCount}/{todosCount}</Typography>
+            <Typography>{Math.round(progress)} %</Typography>
+        </Box>
+    )
+}
+
 const ListList = ({ lists }) => {
     const navigate = useNavigate()
 
@@ -16,20 +42,11 @@ const ListList = ({ lists }) => {
     return (
         <>  
             { lists.map(list =>
-                <Box key={list.id}
-                    onClick={() => handleClick(list.id)}
-                    sx={{
-                        backgroundColor: 'white',
-                        padding: 1,
-                        borderRadius: 5,
-                        marginBottom: 2,
-                        '&:hover': {
-                            backgroundColor: '#f5f5f5'
-                        }
-                    }}
-                    >
-                    <Typography>{list.name}</Typography>
-                </Box>
+                <ListListItem
+                    key={list.id}
+                    list={list}
+                    handleClick={handleClick}
+                />
             )}
         </>
     )
