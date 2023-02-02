a979a3397ac797ae0b4346e2f62481acc742c0e2
uedi <uedi@users.noreply.github.com>
2023-02-02 19:13:52 +0200

    add start and end date to todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import { Box, Typography, Checkbox } from '@mui/material'
+import { format } from 'date-fns'
+
+const DateInfo = ({ todo }) => {
+    const startDate = todo.start ? Date.parse(todo.start) : null
+    const endDate = todo.end ? Date.parse(todo.end) : null
+
+    if(!(startDate || endDate)) {
+        return null
+    }
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}
+        >
+            <Typography>{startDate ? `Start ${format(startDate, 'dd.MM.yyyy')}` : ''}</Typography>
+            <Typography>{endDate ? `Deadline ${format(endDate, 'dd.MM.yyyy')}` : ''}</Typography>
+        </Box>
+    )
+}
 
 const TodoList = ({ todos, updateTodo }) => {
     if(!todos) {
@@ -18,7 +40,7 @@ const TodoList = ({ todos, updateTodo }) => {
             sx={{ maxWidth: 'sm' }}
         >
         {
-            todos.map(todo => 
+            todos.map(todo =>
                 <Box key={todo.id}
                     sx={{
                         backgroundColor: 'white',
@@ -28,16 +50,24 @@ const TodoList = ({ todos, updateTodo }) => {
                         borderRadius: 2,
                         marginBottom: 2,
                         display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center'
+                        flexDirection: 'column'
                     }}
                 >
-                    <Typography
-                        sx={{ flex: 1 }}
-                    >{todo.name}</Typography>
-                    <Checkbox checked={todo.done} onChange={
-                        (event) => handleChange(todo.id, event.target.checked)
-                    } />
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <Typography
+                            sx={{ flex: 1, fontWeight: '600' }}
+                        >{todo.name}</Typography>
+                        <Checkbox checked={todo.done} onChange={
+                            (event) => handleChange(todo.id, event.target.checked)
+                        } />
+                    </Box>
+                    <DateInfo todo={todo} />
                 </Box>
             )
         }
