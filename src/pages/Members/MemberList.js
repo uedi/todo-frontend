b803b1ca5481bc965c398cfe2eb03c33c202ef9e
uedi <uedi@users.noreply.github.com>
2023-02-04 19:21:45 +0200

    memberlist, show group members

---
+++ src/pages/Members/MemberList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+const MemberList = ({ members }) => {
+
+    if(!members) {
+        return null
+    }
+
+    return (
+        <>  
+            { members.map(member =>
+                <Box key={member.id}
+                    sx={{
+                        backgroundColor: 'white',
+                        padding: 2,
+                        borderRadius: 2,
+                        marginBottom: 2
+                    }}
+                    >
+                    <Typography>{member.name}</Typography>
+                </Box>
+            )}
+        </>
+    )
+}
+
+export default MemberList
--- src/pages/Members/index.js
+++ src/pages/Members/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { Button, Typography } from '@mui/material'
+import MemberList from './MemberList'
 
 const Members = () => {
+    const [group, setGroup] = useState()
+    const groups = useSelector(state => state.groups)
+    const params = useParams()
+
+    useEffect(() => {
+        if(groups) {
+            const g = groups.find(g => g.id.toString() === params.id)
+            setGroup(g)
+        } else {
+            setGroup(null)
+        }
+    }, [groups])
+
     return (
-        <div></div>
+        <>
+            <Button>
+                Add member
+            </Button>
+            <Typography variant='h5' sx={{ margin: 2 }}>Group members ({group?.name})</Typography>
+            <MemberList members={group?.users} />
+        </>
     )
 }
 
