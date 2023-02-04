71904a09766c1c04d4a3090aa1908f9d83d35043
uedi <uedi@users.noreply.github.com>
2023-02-04 14:55:33 +0200

    add todolist to group page, update todo functionality

--- src/pages/Group/index.js
+++ src/pages/Group/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import TodoList from '../../components/TodoList'
+import todosService from '../../services/todos'
+import { updateGroupTodo } from '../../reducers/groupsReducer'
 
 const Group = () => {
     const groups = useSelector(state => state.groups)
     const params = useParams()
+    const dispatch = useDispatch()
 
     const group = groups && groups.find(g => g.id.toString() === params.id)
 
@@ -12,10 +17,22 @@ const Group = () => {
         return null
     }
 
+    const handleUpdateTodo = (data) => {
+        const dataToSend = { ...data, groupId: group?.id }
+        todosService.updateTodo(dataToSend)
+        .then(response => {
+            dispatch(updateGroupTodo(group?.id, response))
+        })
+        .catch(error => {
+            console.log('error in update todo', error)
+        })
+    }
+
     return (
-        <div>
-            {group.name}
-        </div>
+        <>
+            <Typography variant='h5' sx={{ margin: 2 }}>Todos ({group.name})</Typography>
+            <TodoList todos={group.todos} updateTodo={handleUpdateTodo} />
+        </>
         
     )
 }
--- src/reducers/groupsReducer.js
+++ src/reducers/groupsReducer.js
@@ -4,6 +4,17 @@ const groupsReducer = (state = null, action) => {
             return action.data
         case 'ADD_GROUP':
             return state ? [...state, action.data] : [action.data]
+        case 'UPDATE_TODO':
+            const groupId = action.data.id
+            const groupToUpdate = state.find(g => g.id === groupId)
+            if(!groupToUpdate || !groupToUpdate?.todos) {
+                return state
+            }
+            const newTodos = groupToUpdate.todos.map(t => t.id === action.data.todo.id ?
+                action.data.todo : t)
+            const newGroup = { ...groupToUpdate, todos: newTodos}
+            return state.map(g => g.id !== groupId ? g
+                : newGroup)
         default:
             return state
     }
@@ -20,6 +31,16 @@ export const addGroup = group => {
     return {
         type: 'ADD_GROUP',
         data: group
+    }
+}
+
+export const updateGroupTodo = (id, todo) => {
+    return {
+        type: 'UPDATE_TODO',
+        data: {
+            id: id,
+            todo: todo
+        }
     }
 }
 
