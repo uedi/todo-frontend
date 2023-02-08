b1df1efe853b27ecdb5abf465ed9b4b9d2dfee62
uedi <uedi@users.noreply.github.com>
2023-02-08 23:17:56 +0200

    show qr code in account page

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Typography, Box } from '@mui/material'
+import QRCode from 'react-qr-code'
 
 const Account = () => {
     const user = useSelector(state => state.user)
     
     return (
         <>
-            <h3>Account</h3>
-            <p>Name: {user?.user?.name}</p>
-            <p>Username: {user?.user?.username}</p>
+            <Typography variant='h5' sx={{ margin: 2 }}>Account</Typography>
+            <Typography>Name: {user?.user?.name}</Typography>
+            <Typography
+                sx={{ marginTop: 2 }}
+            >Username: {user?.user?.username}</Typography>
+            <Box
+                marginTop={3}
+            >
+                <QRCode
+                    value={user?.user.id}
+                    size={150}
+                />
+            </Box>
         </>
     )
 }
