d4e183d2200b3b0848fe9c742488c1d8f6a6ce80
uedi <uedi@users.noreply.github.com>
2023-02-16 20:38:48 +0200

    show colors in grouplist

--- src/pages/Groups/GroupList.js
+++ src/pages/Groups/GroupList.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Box, Typography } from '@mui/material'
-import TodoStatus from '../../components/TodoStatus'
-import PeopleIcon from '@mui/icons-material/People'
+import GroupListItem from './GroupListItem'
 
 const GroupList = ({ groups }) => {
     const navigate = useNavigate()
@@ -18,47 +16,11 @@ const GroupList = ({ groups }) => {
     return (
         <>  
             { groups.map(group =>
-                <Box key={group.id}
-                    onClick={() => handleClick(group.id)}
-                    sx={{
-                        backgroundColor: 'white',
-                        padding: 2,
-                        borderRadius: 2,
-                        marginBottom: 2,
-                        '&:hover': {
-                            backgroundColor: '#f5f5f5'
-                        }
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            justifyContent: 'space-between'
-                        }}
-                    >
-                        <Typography
-                            sx={{ fontWeight: 'bold' }}
-                        >{group.name}</Typography>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <PeopleIcon fontSize='small' />
-                            <Typography
-                                sx={{ marginLeft: 0.3, fontSize: 15 }}
-                            >
-                                {group.users ? group.users.length : 0}
-                            </Typography>
-                        </Box>
-                    </Box>
-                    
-                    <TodoStatus todos={group.todos} />
-                </Box>
+                <GroupListItem
+                    key={group.id}
+                    group={group}
+                    handleClick={handleClick}
+                />
             )}
         </>
     )
---
+++ src/pages/Groups/GroupListItem.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+import { LightenDarkenColor } from 'lighten-darken-color'
+import TodoStatus from '../../components/TodoStatus'
+import PeopleIcon from '@mui/icons-material/People'
+
+const GroupListItem = ({ group, handleClick }) => {
+    const color = group.color || '#ffffff'
+    const darkerColor = LightenDarkenColor(group.color, -10)
+
+    return (
+        <Box
+            onClick={() => handleClick(group.id)}
+            sx={{
+                backgroundColor: color,
+                padding: 2,
+                borderRadius: 2,
+                marginBottom: 2,
+                '&:hover': {
+                    backgroundColor: darkerColor
+                }
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}
+            >
+                <Typography
+                    sx={{ fontWeight: 'bold' }}
+                >{group.name}</Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <PeopleIcon fontSize='small' />
+                    <Typography
+                        sx={{ marginLeft: 0.3, fontSize: 15 }}
+                    >
+                        {group.users ? group.users.length : 0}
+                    </Typography>
+                </Box>
+            </Box>
+            <TodoStatus todos={group.todos} />
+        </Box>
+    )
+}
+
+export default GroupListItem
